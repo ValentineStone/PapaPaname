05047e9537d57d2feed0e3567b01107db0601922
Valentine Stone <sirvalentinestone@gmail.com>
2016-11-03 00:27:05 -0700

    Figured out how to edit and save images ;)

--- src/com/valentine/papapaname/Main.java
+++ src/com/valentine/papapaname/Main.java
@@ -1,16 +1,38 @@
 package com.valentine.papapaname;
 
-import java.awt.Desktop;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
 
 public class Main
 {
 	public static void main(String[] _args) throws IOException
 	{
-		File path = new File(System.getProperty("user.home") + "/PapaPaname/texty.txt");
-		path.mkdirs();
-		Desktop.getDesktop().open(path);
+		Path homePath = Paths.get(System.getProperty("user.home") + "/PapaPaname");
+		
+		Utils.exitIfPathDoesNotExist(homePath);
+		//Desktop.getDesktop().open();
+		
+		Path formImagePath = homePath.resolve("form.png");
+		Path formImagePathDest = homePath.resolve("dest.png");
+		
+		Utils.exitIfPathDoesNotExist(formImagePath);
+		
+		BufferedImage formImage = ImageIO.read(formImagePath.toFile());
+		
+		Graphics g = formImage.getGraphics();
+		g.setColor(Color.GREEN);
+	    g.setFont(g.getFont().deriveFont(30f));
+	    g.drawString("Hello World!", 100, 100);
+	    g.dispose();
+
+	    ImageIO.write(formImage, "png", formImagePathDest.toFile());
 	}
 
 }
